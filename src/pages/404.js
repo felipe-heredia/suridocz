e9e8b494fd61e29a33b7b681b06d15eb4584ab26
felipesuri <felipe@felipesuri.com>
2020-09-15 14:07:36 -0300

    :recycle: modified code style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
-import SEO from '@rocketseat/gatsby-theme-docs/src/components/SEO';
+import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout'
+import SEO from '@rocketseat/gatsby-theme-docs/src/components/SEO'
 
 export default function NotFound() {
   return (
-    <Layout title="Page not found!">
-      <SEO title="404: Not found" />
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Layout title="Página não encontrada!">
+      <SEO title="404: Não encontrada" />
+      <p>Você está tentando acessar uma página que não existe... isso é triste.</p>
       <p>
-        If you&#39;d like to go back to homepage, <Link to="/">click here</Link>
-        .
+        Se você quiser voltar para a página inicial
+        <Link to="/">clique aqui</Link>
       </p>
     </Layout>
-  );
+  )
 }
